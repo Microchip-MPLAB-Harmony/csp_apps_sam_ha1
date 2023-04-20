2117ecf65f6d385d392af621fc1b05a62ba7b72b
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-20 15:36:13 +0530

    [PORT] Regenerated demo for port_port_led_on_off_polling_sam_ha1_xpro
    MH3-79485

--- apps/port/port_led_on_off_polling/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
+++ apps/port/port_led_on_off_polling/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
@@ -71,17 +71,17 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_DIR = 0x1;
-   PORT_REGS->GROUP[0].PORT_OUT = 0x1;
-   PORT_REGS->GROUP[0].PORT_PINCFG[0] = 0x0;
+   PORT_REGS->GROUP[0].PORT_DIR = 0x1U;
+   PORT_REGS->GROUP[0].PORT_OUT = 0x1U;
+   PORT_REGS->GROUP[0].PORT_PINCFG[0] = 0x0U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[0] = 0x0;
+   PORT_REGS->GROUP[0].PORT_PMUX[0] = 0x0U;
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_OUT = 0x8;
-   PORT_REGS->GROUP[1].PORT_PINCFG[3] = 0x6;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x8U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[3] = 0x6U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x0U;
 
 
 }
