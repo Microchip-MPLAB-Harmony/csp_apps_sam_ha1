4bd886d7668e4e75785f5ff84425ec70688c3c3c
Prachi Phadke <prachi.phadke@microchip.com>
2023-04-20 15:36:03 +0530

    [DAC] Regenerated demo for dac_dac_wav_gen_sam_ha1_xpro
    MH3-79478

--- apps/dac/dac_wav_gen/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
+++ apps/dac/dac_wav_gen/firmware/src/config/sam_ha1_xpro/peripheral/port/plib_port.c
@@ -71,15 +71,15 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
-   PORT_REGS->GROUP[0].PORT_PINCFG[2] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PINCFG[2] = 0x1U;
 
-   PORT_REGS->GROUP[0].PORT_PMUX[1] = 0x1;
+   PORT_REGS->GROUP[0].PORT_PMUX[1] = 0x1U;
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_OUT = 0x8;
-   PORT_REGS->GROUP[1].PORT_PINCFG[3] = 0x5;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x8U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[3] = 0x5U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x0;
+   PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x0U;
 
 
 }
