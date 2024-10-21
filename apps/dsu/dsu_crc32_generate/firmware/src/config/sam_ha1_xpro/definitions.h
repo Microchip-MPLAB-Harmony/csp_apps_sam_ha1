f94ae71da4ffd8f3d7cfcc8a405b17cef8d37a2b
Pranjal Das <pranjal.das@microchip.com>
2024-10-21 12:30:50 +0530

    [DSU] Regenerated demo for dsu__dsu_crc32_generate__firmware__sam_ha1_xpro.X
    MH3-102112

--- apps/dsu/dsu_crc32_generate/firmware/src/config/sam_ha1_xpro/definitions.h
+++ apps/dsu/dsu_crc32_generate/firmware/src/config/sam_ha1_xpro/definitions.h
@@ -65,13 +65,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMHA1G16AB"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMH"
-#define DEVICE_SERIES		 "SAMHA1"
+#define DEVICE_NAME          "ATSAMHA1G16AB"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMH"
+#define DEVICE_SERIES        "SAMHA1"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
