0f71b70379ba18ff05b194e83f82a0ebc9643bbe
Pranjal Das <pranjal.das@microchip.com>
2024-10-21 12:27:06 +0530

    [AC] Regenerated demo for ac__ac_sleepwalk_singleshot__firmware__sam_ha1_xpro.X
    MH3-102096

--- apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_ha1_xpro/definitions.h
+++ apps/ac/ac_sleepwalk_singleshot/firmware/src/config/sam_ha1_xpro/definitions.h
@@ -56,8 +56,8 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/pm/plib_pm.h"
-#include "peripheral/ac/plib_ac.h"
 #include "peripheral/rtc/plib_rtc.h"
+#include "peripheral/ac/plib_ac.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -68,13 +68,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMHA1G16AB"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMH"
-#define DEVICE_SERIES		 "SAMHA1"
+#define DEVICE_NAME          "ATSAMHA1G16AB"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMH"
+#define DEVICE_SERIES        "SAMHA1"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 8000000
+#define CPU_CLOCK_FREQUENCY 8000000U
 
 // *****************************************************************************
 // *****************************************************************************
